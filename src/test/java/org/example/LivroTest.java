bd4472aedd9f26300dd962a5367033e5637c58ac
John the Fox <joao.chivite@gmail.com>
2024-09-18 20:50:17 -0300

    Add files via upload
    mais testes e relatorio

--- src/test/java/org/example/LivroTest.java
+++ src/test/java/org/example/LivroTest.java
@@ -1,38 +1,55 @@
 package org.example;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LivroTest {
 
+    private static Livro livro;
+
+    @BeforeAll
+    static void setUp(){
+        livro = new Livro("Java Programming", "Autor A", true);
+    }
+
     @Test
     public void testGetTitulo() {
-        Livro livro = new Livro("Java Programming", "Autor A", true);
+        livro.setTitulo("Java Programming");
         assertEquals("Java Programming", livro.getTitulo());
     }
 
     @Test
     public void testGetAutor() {
-        Livro livro = new Livro("Java Programming", "Autor A", true);
+        livro.setAutor("Autor A");
         assertEquals("Autor A", livro.getAutor());
     }
 
     @Test
     public void testIsDisponivel() {
-        Livro livro = new Livro("Java Programming", "Autor A", true);
         assertTrue(livro.isDisponivel());
     }
 
     @Test
     public void testEmprestarLivro() {
-        Livro livro = new Livro("Java Programming", "Autor A", true);
         livro.emprestar();
         assertFalse(livro.isDisponivel());
     }
 
     @Test
     public void testDevolverLivro() {
-        Livro livro = new Livro("Java Programming", "Autor A", false);
         livro.devolver();
         assertTrue(livro.isDisponivel());
+    }
+
+    @Test
+    public void testSetTitulo(){
+        livro.setTitulo("teste");
+        assertEquals("teste",livro.getTitulo());
+    }
+
+    @Test
+    public void testSetAutor(){
+        livro.setAutor("teste");
+        assertEquals("teste",livro.getAutor());
     }
 }
--- src/test/java/org/example/UsuarioTest.java
+++ src/test/java/org/example/UsuarioTest.java
@@ -1,20 +1,26 @@
 package org.example;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class UsuarioTest {
 
+    private static Usuario usuario;
+    private static Livro livro;
+    @BeforeAll
+    static void setUp(){
+        usuario = new Usuario("João");
+        livro = new Livro("Java Programming", "Autor A", true);
+    }
+
     @Test
     public void testGetNome() {
-        Usuario usuario = new Usuario("João");
         assertEquals("João", usuario.getNome());
     }
 
     @Test
     public void testAlugarLivro() {
-        Livro livro = new Livro("Java Programming", "Autor A", true);
-        Usuario usuario = new Usuario("João");
         usuario.alugarLivro(livro);
         assertFalse(livro.isDisponivel());
         assertEquals(1, usuario.contarLivrosAlugados());
@@ -22,22 +28,53 @@ public class UsuarioTest {
 
     @Test
     public void testDevolverLivro() {
-        Livro livro = new Livro("Java Programming", "Autor A", false);
-        Usuario usuario = new Usuario("João");
         usuario.alugarLivro(livro);
         usuario.devolverLivro(livro);
         assertTrue(livro.isDisponivel());
     }
 
     @Test
+    public void testAlugarLivro2() {
+        Livro livro1 = new Livro("Java Programming0", "Autor Z", true);
+        Livro livro2 = new Livro("Java Programming2", "Autor B", true);
+        Livro livro3 = new Livro("Java Programming3", "Autor C", true);
+        Livro livro4 = new Livro("Java Programming4", "Autor D", true);
+        Livro livro5 = new Livro("Java Programming5", "Autor E", true);
+        usuario.devolverLivro(livro);
+        usuario.alugarLivro(livro1);
+        usuario.alugarLivro(livro2);
+        usuario.alugarLivro(livro3);
+        usuario.alugarLivro(livro4);
+        usuario.alugarLivro(livro5);
+        usuario.alugarLivro(livro);
+        assertTrue(livro.isDisponivel());
+        assertEquals(5, usuario.contarLivrosAlugados());
+        usuario.devolverLivro(livro1);
+        usuario.devolverLivro(livro2);
+        usuario.devolverLivro(livro3);
+        usuario.devolverLivro(livro4);
+        usuario.devolverLivro(livro5);
+    }
+
+    @Test
+    public void testAlugarLivro3() {
+        Usuario usuario2 = new Usuario("Pedro");
+        usuario.devolverLivro(livro);
+        assertTrue(livro.isDisponivel());
+        usuario.alugarLivro(livro);
+        assertFalse(livro.isDisponivel());
+        usuario2.alugarLivro(livro);
+        assertEquals(1, usuario.contarLivrosAlugados());
+        assertEquals(0, usuario2.contarLivrosAlugados());
+    }
+
+    @Test
     public void testContarLivrosAlugados() {
-        Usuario usuario = new Usuario("João");
         assertEquals(0, usuario.contarLivrosAlugados());
     }
 
     @Test
     public void testPodeAlugarMais() {
-        Usuario usuario = new Usuario("João");
         assertTrue(usuario.podeAlugarMais());
     }
 }
